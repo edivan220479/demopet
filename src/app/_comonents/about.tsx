68f23dc7a5ed835d900deb9926246b42d91d19f1
Edivan Fernandes de Oliveira <edivan220479@gmail.com>
2025-02-26 10:21:48 -0300

    criacao do sobre

---
+++ src/app/_comonents/about.tsx
@@ -0,0 +1,7 @@
+export function About() {
+  return (
+    <section>
+        <h1>Sobre</h1>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { About } from "./_comonents/about";
 import { Hero } from "./_comonents/hero";
 
 export default function Home() {
   return (
     <main>
        <Hero/>
+       <About/>
     </main>  
   );
 }
