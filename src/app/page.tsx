58fac66364257d83f5e842b30bb4acd80f5de5b7
Edivan Fernandes de Oliveira <edivan220479@gmail.com>
2025-03-10 09:45:47 -0300

    adicionado services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { About } from "./_comonents/about";
 import { Hero } from "./_comonents/hero";
+import { Services } from "./_comonents/services";
 
 export default function Home() {
   return (
     <main>
        <Hero/>
        <About/>
+       <Services/>
     </main>  
   );
 }
