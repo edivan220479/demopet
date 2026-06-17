159e36cb1c27d1d536f6c342bc68c8e203629a63
edivan220479 <edivan220479@gmail.com>
2026-06-17 19:10:01 -0300

    correção

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/demopet",
+  assetPrefix: "/demopet/",
   images: {
     unoptimized: true,
   },
